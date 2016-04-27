446e1776dc596c87bdec8a1505d776eb0f82a4af
alex poseda <alexposseda@gmil.com>
2016-04-27 13:02:23 +0300

    refactor class

--- core/lib/translit.class.php
+++ core/lib/translit.class.php
@@ -1,35 +1,50 @@
 <?php
 
-class Translit
-{
     /**
-     * Translit constructor.
+     * Class Translit
+     *
+     * replace cyrillic symbols to latin
      */
-    public function __construct()
-    {
-    }
-
-    /**
-     * Translit constructor.
-     * @param string $str
-     */
-    public static function translit($str)
-    {
-        $alphavit = array(
-            /*--*/
-            "а" => "a", "б" => "b", "в" => "v", "г" => "g", "д" => "d", "е" => "e",
-            "ё" => "yo", "ж" => "j", "з" => "z", "и" => "i", "й" => "i", "к" => "k", "л" => "l", "м" => "m",
-            "н" => "n", "о" => "o", "п" => "p", "р" => "r", "с" => "s", "т" => "t",
-            "у" => "y", "ф" => "f", "х" => "h", "ц" => "c", "ч" => "ch", "ш" => "sh", "щ" => "sh",
-            "ы" => "i", "э" => "e", "ю" => "u", "я" => "ya",
-            /*--*/
-            "А" => "A", "Б" => "B", "В" => "V", "Г" => "G", "Д" => "D", "Е" => "E", "Ё" => "Yo",
-            "Ж" => "J", "З" => "Z", "И" => "I", "Й" => "I", "К" => "K", "Л" => "L", "М" => "M",
-            "Н" => "N", "О" => "O", "П" => "P", "Р" => "R", "С" => "S", "Т" => "T", "У" => "Y",
-            "Ф" => "F", "Х" => "H", "Ц" => "C", "Ч" => "Ch", "Ш" => "Sh", "Щ" => "Sh",
-            "Ы" => "I", "Э" => "E", "Ю" => "U", "Я" => "Ya",
-            "ь" => "", "Ь" => "", "ъ" => "", "Ъ" => ""
+    class Translit{
+        private $_alphavit = array(
+            "а" => "a",
+            "б" => "b",
+            "в" => "v",
+            "г" => "g",
+            "д" => "d",
+            "е" => "e",
+            "ё" => "yo",
+            "ж" => "j",
+            "з" => "z",
+            "и" => "i",
+            "й" => "i",
+            "к" => "k",
+            "л" => "l",
+            "м" => "m",
+            "н" => "n",
+            "о" => "o",
+            "п" => "p",
+            "р" => "r",
+            "с" => "s",
+            "т" => "t",
+            "у" => "y",
+            "ф" => "f",
+            "х" => "h",
+            "ц" => "c",
+            "ч" => "ch",
+            "ш" => "sh",
+            "щ" => "sh",
+            "ы" => "i",
+            "э" => "e",
+            "ю" => "u",
+            "я" => "ya"
         );
-        return strtr($str, $alphavit);
+
+        /**
+         * @param string $str
+         * @return string
+         */
+        public function doTranslit($str){
+            return strtr(strtolower($str), $this->_alphavit);
+        }
     }
-}
