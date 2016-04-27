cbb15f762ead5d2c3ca0994cf4d5fc9b7e537ed1
alex poseda <alexposseda@gmil.com>
2016-04-27 11:31:07 +0300

    add db config and core directory path

--- core/config/config.class.php
+++ core/config/config.class.php
@@ -1,7 +1,19 @@
 <?php
     /**
      * class Config
+     *
+     * main project config
      */
     class Config{
+        public $db = [
+            'host' => 'localhost',
+            'name' => 'newaddress_db',
+            'user' => 'root',
+            'password' => '',
+            'prefix' => 'nas'
+        ];
 
+        public $base_url = 'newaddress.local';
+
+        public static $core_dir = 'core/';
     }
