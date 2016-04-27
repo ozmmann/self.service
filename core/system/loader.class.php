af6ff58dda78c2c4fc7aab2b57b16616bbea9b37
alex poseda <alexposseda@gmil.com>
2016-04-27 11:33:09 +0300

    add public method load and some private helpers

---
+++ core/system/loader.class.php
@@ -0,0 +1,64 @@
+<?php
+
+    /**
+     * Class Loader
+     *
+     * if you need load some class, use this class
+     */
+    class Loader{
+        private $_core_dir;
+
+        /**
+         * Loader constructor.
+         */
+        public function __construct(){
+            $this->_core_dir = Config::$core_dir;
+        }
+        
+        /**
+         * @param string $path
+         * @return object
+         */
+        public function load($path){
+            $file = $this->_core_dir.$this->getDir($path).$this->getFile($path);
+            $class_name = $this->getClassName($path);
+
+            if(file_exists($file)){
+                include_once $file;
+            }else{
+                //todo need exception!
+                echo 'file not found!';
+            }
+        }
+
+        /**
+         * @param string $path
+         * @return string
+         */
+        private function getDir($path){
+            $separator_pos = strpos($path, '/');
+            if($separator_pos){
+                return strtolower(substr($path, 0, $separator_pos));
+            }
+            return 'system/';
+        }
+
+        /**
+         * @param string $path
+         * @return string
+         */
+        private function getFile($path){
+            $separator_pos = strpos($path, '/');
+            if($separator_pos){
+                return strtolower(substr($path, $separator_pos)).'.class.php';
+            }
+            return strtolower($path).'.class.php';
+        }
+        private function getClassName($path){
+            $separator_pos = strpos($path, '/');
+            if($separator_pos){
+                return ucfirst(strtolower(substr($path, $separator_pos+1)));
+            }
+            return ucfirst(strtolower($path));
+        }
+    }
