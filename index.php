d0aeb1069a88042cdddef87547af678246676b1e
alex poseda <alexposseda@gmil.com>
2016-04-27 11:33:48 +0300

    include classes and run app

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
+    session_start();
+    require_once 'core/config/config.class.php';
+    require_once 'core/system/loader.class.php';
+    
+    $loader = new Loader();
+    $loader->load('app');
+
+    $app = App::getApp();
+
     
