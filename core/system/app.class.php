61408e84f57ffddad3cc3a51f473378e245a9c5b
alex poseda <alexposseda@gmil.com>
2016-04-27 11:32:07 +0300

    add constructor and getters

--- core/system/app.class.php
+++ core/system/app.class.php
@@ -2,7 +2,45 @@
 
     /**
      * Class App
+     *
+     * general application class
+     * singleton
      */
     class App{
+        private static $app = null;
+        private $_config;
+        private $_loader;
 
+        /**
+         * App constructor.
+         */
+        private function __construct(){
+            global $loader;
+            $this->_loader = $loader;
+            $this->_config = new Config();
+        }
+
+        /**
+         * @return App
+         */
+        public static function getApp(){
+            if(is_null(self::$app)){
+                self::$app = new self;
+            }
+            return self::$app;
+        }
+
+        /**
+         * @return Loader
+         */
+        public function getLoader(){
+            return $this->_loader;
+        }
+
+        /**
+         * @return Config
+         */
+        public function getConfig(){
+            return $this->_config;
+        }
     }
