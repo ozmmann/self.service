47ef184bf74094a4a9ade20c28cba1f3b0b3fb37
alex poseda <alexposseda@gmil.com>
2016-04-29 02:12:45 +0300

    add simple router (version 0.0.1)

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    $_SESSION['role'] = 'admin';
+    $_SESSION['role'] = '';
     require_once 'core/config/config.class.php';
     require_once 'core/system/loader.class.php';
     
@@ -8,32 +8,16 @@
     $loader->load('app');
 
     $app = App::getApp();
-//    if($app->requestMethod() == 'GET'){
-//        $c_name = Config::BASE_CONTROLLER;
-//        if($app->getUrl()->getFirstSegment() == Config::ADMIN_URL and $app->getAuth()->is_admin()){
-//            $c_name = Config::ADMIN_CONTROLLER;
-//        }
-//        $app->getLoader()->load('controller/'.$c_name);
-//
-//        /**
-//         * @var Object $controller
-//         */
-//        $controller = new $c_name;
-//        $controller->run();
-//    }
-    $db = Db::getLink();
-    $sql = new Sql();
-    var_dump($db->sendQuery($sql->select(['id', 'title'])->from(['catalog'])->where(['id', '=' , '1']))->fetch_object());
 
+    $c_name = Config::BASE_CONTROLLER;
+    if($app->getUrl()->getFirstSegment() == Config::ADMIN_URL and $app->getAuth()->is_admin()){
+        $c_name = Config::ADMIN_CONTROLLER;
+    }
+    $app->getLoader()->load('controller/'.$c_name);
 
-    $loader->load('lib/form');
-    $loader->load('lib/validator');
-    $login_data = ['method' => 'post', 'action' => '#', 'button_text' => 'ok',
-                   'fields' => [
-                           ['field' => 'input', 'type' => 'text', 'name' => 'login', 'label' => 'Login', 'text' => 'person', 'required' => '1'],
-                           ['field' => 'input', 'type' => 'text', 'name' => 'password', 'label' => 'Password', 'text' => 'vpn_key', 'required' => '1'],
-                           ['field' => 'input', 'type' => 'checkbox', 'name' => 'remember', 'label' => 'Remember me'],
-                   ]
-    ];
-    $form = new Form($login_data);
-    $form->showForm('layout/admin/form_login.tpl');
+    /**
+     * @var Object $controller
+     */
+    $controller = new $c_name;
+    $controller->run();
+
