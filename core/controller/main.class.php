3e31613817d1e2bc01565f00b57cecf1afbe9618
alex poseda <alexposseda@gmil.com>
2016-04-28 12:54:46 +0300

    add actionAdmin method

--- core/controller/main.class.php
+++ core/controller/main.class.php
@@ -4,9 +4,16 @@
         public function __construct(){
             parent::__construct();
             $this->_action_urlSegmentNum = 0;
+
+            $this->_view = new View('main');
         }
 
         protected function actionIndex(){
             echo 'home';
+        }
+        protected function actionAdmin(){
+            $this->_view->setLayoutDirectory('admin');
+            $this->_view->setLayout('login');
+            $this->_view->render();
         }
     }
